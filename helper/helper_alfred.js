c9a5ee3abb346c4e7c302d49623a8e98ad858b34
Alfred <106579218+AlfredGeraldThendiwijaya@users.noreply.github.com>
2023-03-24 16:45:18 +0700

    Add files via upload

---
+++ helper/helper_alfred.js
@@ -0,0 +1,19 @@
+class Helper {
+  static penambahan(a, b) {
+    return a + b;
+  }
+
+  static pengurangan(a, b) {
+    return a - b;
+  }
+
+  static perkalian(a, b) {
+    return a * b;
+  }
+
+  static pembagian(a, b) {
+    return a / b;
+  }
+}
+
+export default Helper;
---
+++ test/test.js
@@ -0,0 +1,6 @@
+import Helper from "./Helper";
+
+const hasilPenambahan = Helper.penambahan(2, 3);
+const hasilPengurangan = Helper.pengurangan(5, 2);
+const hasilPerkalian = Helper.perkalian(4, 6);
+const hasilPembagian = Helper.pembagian(8, 2);
